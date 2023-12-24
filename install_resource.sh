2438336bde47e60f171c5bbd89c86d017ef6bb8f
NaturalSelect <2145973003@qq.com>
2023-12-25 00:40:15 +0800

    chore: remove unused logs
    Signed-off-by: NaturalSelect <2145973003@qq.com>

--- install_resource.sh
+++ install_resource.sh
@@ -4,7 +4,7 @@ echo "$(pwd)"
 mv chromium77-client.tar.gz ./resources/app/chromium77-client.tar.gz
 
 pushd ./resources/app/
-tar -zxvf chromium77-client.tar.gz
+tar -zxf chromium77-client.tar.gz
 rm chromium77-client.tar.gz
 mv ./noname-chromium77-client/* ./
 rm -r ./noname-chromium77-client
